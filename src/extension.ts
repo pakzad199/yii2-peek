5c39ac161cea24d90e689c744989c899c10c1a4d
Mahdi <pakzad199@gmail.com>
2022-07-02 20:25:01 +0430

    fix pattern of variable names

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,11 @@ import * as path from 'path';
 export function activate(context: vscode.ExtensionContext) {
 	
 	let config = vscode.workspace.getConfiguration('file_peek');
-	let active_languages = (config.get('activeLanguages') as Array<string>);
-	let search_file_extensions = (config.get('searchFileExtensions') as Array<string>);
+	let activeLanguages = (config.get('activeLanguages') as Array<string>);
+	let searchFileExtensions = (config.get('searchFileExtensions') as Array<string>);
 
 
-	const peek_filter: vscode.DocumentFilter[] = active_languages.map((language) => {
+	const peekFilter: vscode.DocumentFilter[] = activeLanguages.map((language) => {
 		return {
 		   language: language,
 		   scheme: 'file'
@@ -17,8 +17,8 @@ export function activate(context: vscode.ExtensionContext) {
 	 });
 
 	 context.subscriptions.push(
-		vscode.languages.registerDefinitionProvider(peek_filter,
-					   new PeekFileDefinitionProvider(search_file_extensions))
+		vscode.languages.registerDefinitionProvider(peekFilter,
+					   new PeekFileDefinitionProvider(searchFileExtensions))
 	 );
 
 }
@@ -36,95 +36,75 @@ class PeekFileDefinitionProvider implements vscode.DefinitionProvider {
  
 	getPotentialPaths(lookupPath: string): string[] 
 	{
-	   let potential_paths: string[] = [lookupPath];
+	   let potentialPaths: string[] = [lookupPath];
  
 	   // Add on list where we just add the file extension directly
 	   this.fileSearchExtensions.forEach((extStr) => {
-		  potential_paths.push(lookupPath + extStr);
+		  potentialPaths.push(lookupPath + extStr);
 	   });
  
 	   // if we have an extension, then try replacing it.
-	   let parsed_path = path.parse(lookupPath);
-	   if (parsed_path.ext !== "") {
+	   let parsedPath = path.parse(lookupPath);
+	   if (parsedPath.ext !== "") {
 		  this.fileSearchExtensions.forEach((extStr) => {
-			 const new_path = path.format({
-				base: parsed_path.name + extStr,
-				dir: parsed_path.dir,
+			 const newPath = path.format({
+				base: parsedPath.name + extStr,
+				dir: parsedPath.dir,
 				ext: extStr,
-				name: parsed_path.name,
-				root: parsed_path.root
+				name: parsedPath.name,
+				root: parsedPath.root
 			 });
-			 potential_paths.push(new_path);
+			 potentialPaths.push(newPath);
 		  });
 	   }
  
-	   return potential_paths;
+	   return potentialPaths;
 	}
  
 	provideDefinition(document: vscode.TextDocument,
 					  position: vscode.Position,
-					  token: vscode.CancellationToken): vscode.Definition 
+					  token: vscode.CancellationToken): vscode.Definition | undefined
 	{
 	   // todo: make this method operate async
-	   let working_dir = path.dirname(document.fileName);
+	   let workingDir = path.dirname(document.fileName);
 	   let word = document.getText(document.getWordRangeAtPosition(position));
 	   let line = document.lineAt(position);
  
-	//    console.log('====== peek-file definition lookup ===========');
-	//    console.log('word: ' + word);
-	//    console.log('working_dir: ' + working_dir);
-	//    console.log('line: ' + line.text);
-	//    console.log('document: ' + document.fileName);
-	//    console.log('typeof document: ' + document.fileName + '/../');
+	   let fileName = path.basename(document.fileName, '.php').replace('Controller', '').toLowerCase();
 
-	   let file_name = path.basename(document.fileName, '.php').replace('Controller', '').toLowerCase();
-
-	   let remove_extra_address = document.fileName.slice(
+	   let removeExtraAddress = document.fileName.slice(
 		document.fileName.indexOf('controllers')
 	   );
 
 	   // generate view path
-	   let view_path = document.fileName.replace(remove_extra_address, '') + 'views/' + file_name;
-	//    console.log('view_path: ' + view_path);
+	   let viewPath = document.fileName.replace(removeExtraAddress, '') + 'views/' + fileName;
 
 	   // We are looking for strings with filenames
 	   // - simple hack for now we look for the string with our current word in it on our line
 	   //   and where our cursor position is inside the string
-	   let re_str = `\"(.*?${word}.*?)\"|\'(.*?${word}.*?)\'`;
-	   let match = line.text.match(re_str);
- 
-	//    console.log('re_str: ' + re_str);
-	//    console.log("   Match: ", match);
+	   let match = line.text.match(`\"(.*?${word}.*?)\"|\'(.*?${word}.*?)\'`);
  
 	   if (null !== match) {
-		  let potential_fname = match[1] || match[2];
-		  let match_start = match.index;
-		  let match_end   = match.index + potential_fname.length;
+		  let potentialFname = match[1] || match[2];
+		  let matchStart = match.index ?? 0;
+		  let matchEnd   = matchStart + potentialFname.length;
  
 		  // Verify the match string is at same location as cursor
-		  if((position.character >= match_start) &&
-			 (position.character <= match_end))
+		  if((position.character >= matchStart) &&
+			 (position.character <= matchEnd))
 		  {
-			 let full_path   = path.resolve(view_path, potential_fname);
-			//  console.log(" Match: ", match);
-			//  console.log(" Fname: " + potential_fname);
-			//  console.log("  Full: " + full_path);
+			 let fullPath   = path.resolve(viewPath, potentialFname);
  
 			 // Find all potential paths to check and return the first one found
-			 let potential_fnames = this.getPotentialPaths(full_path);
-			//  console.log(" potential fnames: ", potential_fnames);
+			 let potentialFnames = this.getPotentialPaths(fullPath);
  
-			 let found_fname = potential_fnames.find((fname_full) => {
-				//console.log(" checking: ", fname_full);
-				return fs.existsSync(fname_full);
+			 let foundFname = potentialFnames.find((fnameFull) => {
+				return fs.existsSync(fnameFull);
 			 });
-			 if (found_fname != null) {
-				// console.log('found: ' + found_fname);
-				return new vscode.Location(vscode.Uri.file(found_fname), new vscode.Position(0, 1));
+			 if (foundFname != null) {
+				return new vscode.Location(vscode.Uri.file(foundFname), new vscode.Position(0, 1));
 			 }
 		  }
 	   }
- 
-	   return null;
 	}
  }
